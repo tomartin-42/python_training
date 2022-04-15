7d15544dd7c1317380680c3e6fecbca81a14c6ea
tomartin-42 <tomasmartinezcurz@gmail.com>
2022-04-15 12:18:38 +0200

    end day00 ex01

--- day00/ex01/exec.py
+++ day00/ex01/exec.py
@@ -6,7 +6,7 @@
 #    By: tomartin <marvin@42.fr>                    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2022/04/14 17:26:44 by tomartin          #+#    #+#              #
-#    Updated: 2022/04/14 20:15:57 by tomartin         ###   ########.fr        #
+#    Updated: 2022/04/15 12:18:26 by tomartin         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -15,6 +15,7 @@ import sys
 if len(sys.argv) == 1:
     print("Error:")
     quit(1)
-for word in reversed(sys.argv[1::-1]):
-    new_argv = word.swapcase()
-    print(new_argv)
+new_arg = str()
+for word in reversed(sys.argv[1:]):
+    new_arg = new_arg + (word.swapcase()[::-1]) + " "
+print(new_arg[:-1])
